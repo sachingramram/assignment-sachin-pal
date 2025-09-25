308e449cdd89df0dfbaf9f085e8f58ff44a7fc9a
Sachin Pal <sachinpal9352@gmial.com>
2025-09-25 14:10:22 +0530

    improved next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'randomuser.me', pathname: '/api/portraits/**' },
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' }
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'i.pravatar.cc', pathname: '/**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/u/**' }
     ]
   }
 }
